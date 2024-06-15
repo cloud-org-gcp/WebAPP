46a24af48917be5e55232c5bf829b17023a89828
Viswanath Raju Indukuri <indukuri.v@northeastern.edu>
2024-06-15 18:37:03 -0400

    Feat: FastAPI

--- 01/database.py
+++ 01/database.py
@@ -1,9 +1,18 @@
 # database.py
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
 
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost/apitest')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

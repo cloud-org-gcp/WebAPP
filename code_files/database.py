3d120fbf485e207cc2dbccc30d1136442d6c3452
Viswanath Raju Indukuri <144731305+ViswanathRajuIndukuri@users.noreply.github.com>
2024-06-19 19:02:07 -0400

    Viswanath (#3)
    * Update .gitignore
    
    * Feat: FastAPI
    
    * update gitignore
    
    * update folder name and gitignore
    
    * Delete myenv directory
    
    * Added basic auth
    
    * adding CI .yml file
    
    * update pytest
    
    * updates
    
    * updates
    
    * updates
    
    * completed

--- code_files/database.py
+++ code_files/database.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost/apitest')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost/postgres')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
